15539e48c85495c1b0f8e632fe7e9d34a3eef587
TownOfGoog <95412264+TownOfGoog@users.noreply.github.com>
2023-05-03 07:32:20 +0000

    Request / Response Experiments 1 | fertig

--- express_helloworld/index.js
+++ express_helloworld/index.js
@@ -17,5 +17,39 @@ app.listen(port, () => {
 });
 
 app.get("/zli", (req, res) => {
-    res.send(());
+    window.location.replace("zli.ch");
+});
+
+app.get("/name", (req, res) => {
+    let num = Math.floor(Math.random()*10);
+    let names = ["Tim", "Tom", "Till", "Thomas", "Troy", "Manuel", "Elias", "Allessandro", "Joel", "Leli"]
+    res.send(names[num]);
+});
+
+app.get("/html", (req, res) => {
+    res.sendFile('/workspaces/M295 Backend/express_helloworld/Aufgabe4.html')
+});
+
+app.get("/image", (req, res) => {
+    res.sendFile('/workspaces/M295 Backend/express_helloworld/PUCK.png')
+});
+
+app.get("/teapot", (req, res) => {
+    res.sendStatus(418)
+});
+
+app.get("/user-agent", (req, res) => {
+    res.send.req.header(user-agent)
+});
+
+app.get("/secret", (req, res) => {
+    res.sendStatus(403)
+});
+
+app.get("/xml", (req, res) => {
+    res.sendFile("/workspaces/M295 Backend/express_helloworld/Hotel sterne=5.xml")
+});
+
+app.get("/me", (req, res) => {
+    res.sendFile("/workspaces/M295 Backend/express_helloworld/me.json")
 });

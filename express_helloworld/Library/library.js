2b7e9a59094d4e21f86ff1ed4739e8670f13f040
TownOfGoog <95412264+TownOfGoog@users.noreply.github.com>
2023-05-04 12:36:37 +0000

    Library Aufgaben 2 zum grossteil erarbeitet

--- express_helloworld/Library/library.js
+++ express_helloworld/Library/library.js
@@ -31,6 +31,23 @@ let books=[{
 }
 ]
 
+let lends = [
+    {
+        "LendID": "1",
+        "CustomerID": "1",
+        "BookID": "1",
+        "borrowed_at": "2011-11-11",
+        "returned_at": "2022-02-22"
+    },
+    {
+        "LendID": "2",
+        "CustomerID": "3",
+        "BookID": "323",
+        "borrowed_at": "2023-06-04",
+        "returned_at": null
+    }
+]
+
 
 //GUT
 app.get("/books", (req, res) => {
@@ -108,3 +125,71 @@ export function remove(isbn){
 
 
 //-- .. .- -- .. .- -- .. .- -- .. .- -- .. .- -- .. .- -- .. .- -- .. .- -- .. .- -- .. .- -- .. .- -- .. .- -- .. .- -- .. .-
+
+
+function findALLLends(){
+    return lends
+}
+
+app.get("/lend", (req, res) => {
+    res.send(findALLLends())
+})
+
+app.get("/lend/:lendid", (req, res) =>{
+    let id = res.params.lendid
+    res.send(findONELend(id))
+})
+
+
+app.put("/lend/:lendid", (req, res) => {
+    let lendid = req.params.lendid
+    var currentDate = new Date();
+    var currentDateJson = currentDate.toJSON();
+    let newlend = 
+    {
+        "LendID": lendid,
+        "CustomerID": "6",
+        "BookID": "123",
+        "borrowed_at": currentDateJson,
+        "returned_at": null
+    }
+    
+    replaceLend(newlend)
+    res.send(findALLLends())
+})
+
+
+
+app.post("/lend", (req, res) => {
+    var currentDate = new Date();
+    var currentDateJson = currentDate.toJSON();
+    let lendNew = 
+        {
+            "LendID": 7,
+            "CustomerID": "9",
+            "BookID": "123",
+            "borrowed_at": currentDateJson,
+            "returned_at": null
+        }
+    
+    insertLend(lendNew)
+    
+    res.sendStatus(201);
+})
+
+function findONELend(LendID) {
+    return lends.find(lend => lend.LendID === LendID)
+}
+
+function replaceLend (book) {
+    lends = lends.map((b) => b.LendID == book.LendID ? book : b);
+} 
+
+
+
+export function insertLend (book) {
+    lends = [... lends, book];
+}
+
+
+
